34ff66b9231aceb18267e7aaa1a326427e384179
setare <setarehsaboory@gmail.com>
2022-11-04 00:58:50 +0100

    frankfurter api set

--- script.js
+++ script.js
@@ -9,29 +9,25 @@
 // .............................................................................................................................
 const symbolsBtn = document.querySelector("#symbols-btn");
 const allCurrencies = document.querySelector(".all-currencies");
-var myHeaders = new Headers();
-myHeaders.append("apikey", "rQl0x7RRuSlPPwTSMLuRdkIwA1miRAWf");
 
-var requestOptions = {
-  method: "GET",
-  redirect: "follow",
-  headers: myHeaders,
-};
+const host = 'api.frankfurter.app';
 async function getSymbols() {
-  const res = await fetch("https://openexchangerates.org/api/currencies.json?app_id=3e2d226508c34e5baffca2d13064fe91")
+  const res = await fetch("https://api.frankfurter.app/currencies")
+    .then(resp => resp.json())
+    .then((data) => {
+      displaySymbols(data);
+    });
 
-  const data = await res.json();
-  displaySymbols(data);
 }
 
 function displaySymbols(symbols) {
   let enteries = Object.entries(symbols);
+  console.log(enteries.length);
   for (let i = 0; i < enteries.length; i++) {
     const btn = document.createElement('button');
-    btn.innerHTML = enteries[i][1];
+    btn.innerHTML = enteries[i][0];
     allCurrencies.appendChild(btn);
 
   }
 }
-// symbolsBtn.addEventListener('click', );
 getSymbols();
