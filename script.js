5fe12fbe3a2c3d4549a050839c948c61c73036ba
setare <setarehsaboory@gmail.com>
2022-11-04 19:39:16 +0100

    card

--- script.js
+++ script.js
@@ -4,19 +4,20 @@ const symbolsBtn = document.querySelector("#symbols-btn");
 const convertBtn = document.querySelector(".convert-btn");
 const allCurrencies = document.querySelector(".all-currencies");
 const cForm = document.querySelector("#convert-Form");
+const card = document.querySelector(".c-card");
 const watchList = document.querySelector("#watch-list");
 const host = 'api.frankfurter.app';
-//how to handle below func?
+//how to handle below func? to make it more readable
 //seperate each if as a seperated function? ooor what ?
 async function getCurrency(flag, from, to, amount) { //what was the name of these functions? :/
-  if (flag === 1) { //to get all currencies
+  if (flag === 1) {
     fetch(`https://${host}/currencies`)
       .then(resp => resp.json())
       .then((data) => {
         displaySymbols(data);
       })
   }
-  if (flag === 2) { //to convert
+  if (flag === 2) {
     fetch(`https://${host}/latest?amount=${amount}&from=${from}&to=${to}`)
       .then(resp => resp.json())
       .then((data) => {
@@ -26,14 +27,13 @@ async function getCurrency(flag, from, to, amount) { //what was the name of thes
 
 }
 function displayConvert(data) {
-  console.log(`${data.amount} ${data.base} = ${Object.values(data.rates)[0]} ${Object.keys(data.rates)[0]}`);
-
+  card.innerHTML = `${data.amount} ${data.base} = ${Object.values(data.rates)[0]} ${Object.keys(data.rates)[0]}`;
 }
 function displaySymbols(symbols) {
   let enteries = Object.entries(symbols);
   console.log(enteries.length);
   for (let i = 0; i < enteries.length; i++) {
-    const btn = document.createElement('button');
+    const btn = document.createElement('span');
     btn.innerHTML = enteries[i][0];
     allCurrencies.appendChild(btn);
   }
@@ -45,10 +45,8 @@ symbolsBtn.addEventListener('click', function () {
 });
 convertBtn.addEventListener('click', function () {
   event.preventDefault();
+  card.classList.add("active");
   const from = cForm.elements.from.value;
   const to = cForm.elements.to.value;
-  console.log(from + to);
   getCurrency(2, from, to, 1);
 })
-
-console.log(screens.length);
