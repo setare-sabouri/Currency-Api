142039dc32cfbb2de9a1846548cf3c393b87218d
setare <setarehsaboory@gmail.com>
2022-11-04 01:19:19 +0100

    initialize  fetch of 2 btns

--- script.js
+++ script.js
@@ -8,15 +8,23 @@
 // /fluctuation Returns fluctuation data between two specified dates for all available or a specific set of currencies.
 // .............................................................................................................................
 const symbolsBtn = document.querySelector("#symbols-btn");
+const convertBtn = document.querySelector("#convert-btn");
 const allCurrencies = document.querySelector(".all-currencies");
 
 const host = 'api.frankfurter.app';
-async function getSymbols() {
-  const res = await fetch("https://api.frankfurter.app/currencies")
-    .then(resp => resp.json())
-    .then((data) => {
-      displaySymbols(data);
-    });
+async function getCurrency(flag) {
+  if (flag === 1) { //to get all currencies
+    const res = await fetch(`https://${host}/currencies`);
+    const data = await res.json();
+    displaySymbols(data);
+  }
+  if (flag === 2) {
+    fetch(`https://${host}/latest?amount=10&from=GBP&to=USD`)
+      .then(resp => resp.json())
+      .then((data) => {
+        console.log(`10 GBP = ${data.rates.USD} USD`);
+      });
+  }
 
 }
 
@@ -27,7 +35,11 @@ function displaySymbols(symbols) {
     const btn = document.createElement('button');
     btn.innerHTML = enteries[i][0];
     allCurrencies.appendChild(btn);
-
   }
 }
-getSymbols();
+symbolsBtn.addEventListener('click', function () {
+  getCurrency(1);
+});
+convertBtn.addEventListener('click', function () {
+  getCurrency(2);
+})
