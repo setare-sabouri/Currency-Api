2c296605f2551d7a4345fd709f85e4d0d8ef9a79
setare <setarehsaboory@gmail.com>
2022-11-07 02:25:11 +0100

    New UI

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 "use strict";
-const screens = document.querySelectorAll('.screen');
+import { app, db } from "./firebase.js";
 const symbolsBtn = document.querySelector("#symbols-btn");
 const convertBtn = document.querySelector(".convert-btn");
 const allCurrencies = document.querySelector(".all-currencies");
 const cForm = document.querySelector("#convert-Form");
 const card = document.querySelector(".c-card");
-const watchList = document.querySelector("#watch-list");
+const SearchDbtn = document.querySelector('#date-search-btn');
+const dateForm = document.querySelector("#date-form");
 const host = 'api.frankfurter.app';
 //how to handle below func? to make it more readable
 //seperate each if as a seperated function? ooor what ?
@@ -24,29 +25,48 @@ async function getCurrency(flag, from, to, amount) { //what was the name of thes
         displayConvert(data);
       })
   }
-
 }
 function displayConvert(data) {
   card.innerHTML = `${data.amount} ${data.base} = ${Object.values(data.rates)[0]} ${Object.keys(data.rates)[0]}`;
 }
 function displaySymbols(symbols) {
+  allCurrencies.innerHTML = "";
   let enteries = Object.entries(symbols);
-  console.log(enteries.length);
+  console.log("Updated");
   for (let i = 0; i < enteries.length; i++) { //add label later for value of each
-    const btn = document.createElement('span');
-    btn.innerHTML = enteries[i][0];
-    allCurrencies.appendChild(btn);
+    const span = document.createElement('span'); //and rates and date
+    span.innerHTML = enteries[i][0];
+    allCurrencies.appendChild(span);
   }
 }
+function displayByDate(data) {
+  console.log(data);
 
+}
 symbolsBtn.addEventListener('click', function () {
-  screens[0].classList.add('up');
   getCurrency(1);
 });
+
 convertBtn.addEventListener('click', function () {
   event.preventDefault();
-  card.classList.add("active");
   const from = cForm.elements.from.value;
   const to = cForm.elements.to.value;
   getCurrency(2, from, to, 1);
+  card.classList.add("active");
 })
+SearchDbtn.addEventListener('click', function () {
+  event.preventDefault();
+  const fromDate = dateForm.elements.fromDate.value;
+  const toDate = dateForm.elements.toDate.value;
+  fetch(`https://${host}/${fromDate}..${toDate}`)
+    .then(resp => resp.json())
+    .then((data) => {
+      displayByDate(data);
+    })
+})
+// dateinput.addEventListener('input', function () {  this is for one date only if needed
+//   const dateFrom = dateinput.value;
+//   console.log(dateFrom);
+
+// })
+getCurrency(1);
